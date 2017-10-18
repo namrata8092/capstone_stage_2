f0992aa690e5d83a3686c58b3c7d16f0c5ab5f5f
namrata8092 <namrata.shah8092@gmail.com>
2017-10-17 23:06:04 -0400

    added basic structure of project

---
+++ app/src/main/java/com/nds/pmc/views/activities/LauncherActivity.java
@@ -0,0 +1,20 @@
+package com.nds.pmc.views.activities;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.nds.pmc.R;
+
+/**
+ * Created by Namrata on 10/17/2017.
+ */
+
+public class LauncherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+    }
+}
