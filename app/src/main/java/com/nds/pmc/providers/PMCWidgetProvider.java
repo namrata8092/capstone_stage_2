4d77abca538b57d25442f4d4ab0cbb8d1a03f50f
Shah, Namrata <namrata.shah@verizonwireless.com>
2018-02-12 20:44:22 -0500

    populating category icon in widget

--- app/src/main/java/com/nds/pmc/providers/PMCWidgetProvider.java
+++ app/src/main/java/com/nds/pmc/providers/PMCWidgetProvider.java
@@ -4,12 +4,15 @@ import android.annotation.TargetApi;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.widget.RemoteViews;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.AppWidgetTarget;
 import com.nds.pmc.R;
 import com.nds.pmc.common.Constants;
 import com.nds.pmc.model.Place;
@@ -19,6 +22,7 @@ import com.nds.pmc.views.activities.CategorySearchDetailActivity;
 
 import java.util.ArrayList;
 
+import static com.google.android.gms.internal.zzagr.runOnUiThread;
 import static com.nds.pmc.services.PMCWidgetService.startPMCWidgetService;
 
 /**
@@ -34,11 +38,13 @@ public class PMCWidgetProvider extends AppWidgetProvider {
     private static final String PLACE_BUNDLE="placeBundle";
     private static final String PLACE_LIST="placeList";
     private static int PLACE_INDEX = 0;
+    private static Context mContext;
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
         LogUtil.d(TAG,"onUpdate called "+ PLACE_INDEX);
+        mContext = context;
         startPMCWidgetService(context);
     }
 
@@ -46,6 +52,7 @@ public class PMCWidgetProvider extends AppWidgetProvider {
     public static void updateWidget(Context context, AppWidgetManager appWidgetManager,
                                     int appWidgetIds, ArrayList<Place> mPlaces) {
         LogUtil.d(TAG,"updateWidget called "+ PLACE_INDEX);
+        mContext = context;
         RemoteViews views = getPlacesGridView(context, appWidgetIds, mPlaces);
         appWidgetManager.updateAppWidget(appWidgetIds, views);
     }
@@ -53,12 +60,27 @@ public class PMCWidgetProvider extends AppWidgetProvider {
     private static RemoteViews getPlacesGridView(Context context, int appWidgetIds,
                                                  ArrayList<Place> places) {
         LogUtil.d(TAG,"getPlacesGridView");
+
         mPlaces = places;
-        Place selectedPlace = mPlaces.get(PLACE_INDEX);
+        final Place selectedPlace = mPlaces.get(PLACE_INDEX);
         selectedPlace.setWidgetEntry(true);
-        RemoteViews view = new RemoteViews(context.getPackageName(), R.layout.widget_grid_cell);
+        final RemoteViews view = new RemoteViews(context.getPackageName(), R.layout.widget_grid_cell);
         view.setTextViewText(R.id.placeName, selectedPlace.getName());
-//        view.setImageViewResource(R.id.categoryType, R.drawable.airport);
+        final AppWidgetTarget appWidgetTarget = new AppWidgetTarget( context, view, R.id.categoryType, appWidgetIds);
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Glide.with(mContext.getApplicationContext()).load(selectedPlace.getIconImage())
+                        .asBitmap()
+                        .into(appWidgetTarget);
+
+
+                pushWidgetUpdate(mContext, view);
+            }
+        });
+
+
 
         Intent intent = new Intent(context, PlaceRemoteViewService.class);
         intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds);
@@ -77,7 +99,11 @@ public class PMCWidgetProvider extends AppWidgetProvider {
         return view;
     }
 
-
+    public static void pushWidgetUpdate(Context context, RemoteViews rv) {
+        ComponentName myWidget = new ComponentName(context, PMCWidgetProvider.class);
+        AppWidgetManager manager = AppWidgetManager.getInstance(context);
+        manager.updateAppWidget(myWidget, rv);
+    }
 
     @Override
     public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager,
