0a0c4ca18802b6bacf06cbc3a04c2693b4529487
Shah, Namrata <namrata.shah@verizonwireless.com>
2018-03-17 17:11:46 -0400

    resolved favorite screen landscape mode issue

--- app/src/main/java/com/nds/pmc/views/fragments/FavoritePlaceListFragment.java
+++ app/src/main/java/com/nds/pmc/views/fragments/FavoritePlaceListFragment.java
@@ -3,13 +3,17 @@ package com.nds.pmc.views.fragments;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.database.Cursor;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityOptionsCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.LoaderManager;
 import android.support.v4.content.AsyncTaskLoader;
@@ -27,6 +31,7 @@ import com.nds.pmc.R;
 import com.nds.pmc.common.Constants;
 import com.nds.pmc.dbo.PlaceContract;
 import com.nds.pmc.model.Place;
+import com.nds.pmc.util.DeviceUtil;
 import com.nds.pmc.views.adapters.SearchResultListAdapter;
 
 import java.util.ArrayList;
@@ -36,7 +41,7 @@ import java.util.List;
  * Created by Namrata Shah on 1/15/2018.
  */
 
-public class FavoritePlaceListFragment extends Fragment implements SharedPreferences.OnSharedPreferenceChangeListener, AdapterView.OnItemClickListener {
+public class FavoritePlaceListFragment extends Fragment implements SharedPreferences.OnSharedPreferenceChangeListener, AdapterView.OnItemClickListener, Handler.Callback {
 
     private static final int FAVORITE_PLACE_LOADER_ID = 20;
     private LoaderManager.LoaderCallbacks<Cursor> mFavoritePlaceFromDBLoaderListener;
@@ -57,6 +62,9 @@ public class FavoritePlaceListFragment extends Fragment implements SharedPrefere
     };
     private SearchResultListAdapter placeListAdapter;
     private RecyclerView searchResultRecyclerView;
+    private View mRootView;
+    private int mSelectedPlaceIndex = 0;
+    private Handler mHandler;
 
     @Override
     public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
@@ -81,13 +89,13 @@ public class FavoritePlaceListFragment extends Fragment implements SharedPrefere
             @Override
             public Loader<Cursor> onCreateLoader(int id, Bundle args) {
                 return new AsyncTaskLoader<Cursor>(mContext) {
-                    Cursor favoriteMoviesCursor = null;
+                    Cursor favoritePlaceCursor = null;
 
                     @Override
                     protected void onStartLoading() {
                         displayProgressBar();
-                        if (favoriteMoviesCursor != null) {
-                            deliverResult(favoriteMoviesCursor);
+                        if (favoritePlaceCursor != null) {
+                            deliverResult(favoritePlaceCursor);
                         } else {
                             forceLoad();
                         }
@@ -130,6 +138,11 @@ public class FavoritePlaceListFragment extends Fragment implements SharedPrefere
             }
         };
 
+        if(savedInstanceState!=null && savedInstanceState.containsKey(Constants.SELECTED_PLACE_INDEX_KEY)){
+            mSelectedPlaceIndex = savedInstanceState.getInt(Constants.SELECTED_PLACE_INDEX_KEY);
+        }
+
+        mHandler = new Handler(this);
     }
 
     @Override
@@ -154,32 +167,74 @@ public class FavoritePlaceListFragment extends Fragment implements SharedPrefere
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_favorite_places, container, false);
+        checkTwoPanelLayout(rootView);
         mProgress = (ProgressBar) rootView.findViewById(R.id.progress);
         mErrorMsgTV = (TextView) rootView.findViewById(R.id.noPlace);
         searchResultRecyclerView = (RecyclerView) rootView.findViewById(R.id.searchResultRecyclerView);
         LinearLayoutManager gridLayoutManager = new LinearLayoutManager(getContext());
         searchResultRecyclerView.setLayoutManager(gridLayoutManager);
+        mRootView = rootView;
         return rootView;
     }
 
     @Override
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+        if(mFavoritePlaceList!=null && mFavoritePlaceList.size() > 0 && !mFavoritePlaceList.isEmpty()) {
+            Place place = mFavoritePlaceList.get(position);
+            mSelectedPlaceIndex = position;
+            displayPlaceDetails(place);
+        }
+    }
+
+    @Override
+    public void onSaveInstanceState(@NonNull Bundle outState) {
+        outState.putInt(Constants.SELECTED_PLACE_INDEX_KEY, mSelectedPlaceIndex);
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        if (newConfig.orientation == Configuration.ORIENTATION_PORTRAIT){
+            DeviceUtil.setTwoPanelLayout(false);
+        }
+    }
+
+    private void displayPlaceDetails(Place place) {
+        if(!DeviceUtil.isTwoPanelLayout()){
+            displayDetailActivity(place);
+        }else{
+            displayDetailFragment(place);
+        }
+    }
+
+    private void displayDetailFragment(Place place) {
+        SearchResultDetailFragment searchResultDetailFragment = SearchResultDetailFragment.newInstance(place);
+        getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.detail_container, searchResultDetailFragment).commit();
+    }
+
+    private void displayDetailActivity(Place place) {
         Intent detailIntent = new Intent();
         detailIntent.setAction(Constants.ACTION_SEARCH_RESULT_DETAIL);
-        detailIntent.putExtra(Constants.PLACE_BUNDLE_KEY, mFavoritePlaceList.get(position));
-        startActivity(detailIntent);
+        detailIntent.putExtra(Constants.PLACE_BUNDLE_KEY, place);
+        ActivityOptionsCompat activityOptionsCompat = ActivityOptionsCompat.makeSceneTransitionAnimation(getActivity(),
+                mRootView.findViewById(R.id.title), getString(R.string.shared_transition_name_from_list_to_detail));
+        startActivity(detailIntent, activityOptionsCompat.toBundle());
     }
 
     private void setFavoritePlacesAdapter() {
         placeListAdapter = new SearchResultListAdapter(
                 getContext(), mFavoritePlaceList, this);
         searchResultRecyclerView.setAdapter(placeListAdapter);
+        if(DeviceUtil.isTwoPanelLayout()){
+            mHandler.sendEmptyMessage(Constants.DISPLAY_PLACE_DETAIL_MSG);
+        }
     }
 
     private void fetchFavoritePlacesFromDB() {
         LoaderManager loaderManager = getLoaderManager();
-        Loader<Cursor> favoriteMovieLoader = loaderManager.getLoader(FAVORITE_PLACE_LOADER_ID);
-        if (favoriteMovieLoader == null) {
+        Loader<Cursor> favoritePlaceLoader = loaderManager.getLoader(FAVORITE_PLACE_LOADER_ID);
+        if (favoritePlaceLoader == null) {
             loaderManager.initLoader(FAVORITE_PLACE_LOADER_ID, null, mFavoritePlaceFromDBLoaderListener);
         } else {
             loaderManager.restartLoader(FAVORITE_PLACE_LOADER_ID, null, mFavoritePlaceFromDBLoaderListener);
@@ -227,5 +282,17 @@ public class FavoritePlaceListFragment extends Fragment implements SharedPrefere
         mProgress.setVisibility(View.GONE);
     }
 
+    private void checkTwoPanelLayout(View rootView) {
+        if (rootView.findViewById(R.id.detail_container) != null) {
+            DeviceUtil.setTwoPanelLayout(true);
+        }
+    }
 
+    @Override
+    public boolean handleMessage(Message msg) {
+        if (msg.what == Constants.DISPLAY_PLACE_DETAIL_MSG && mFavoritePlaceList != null && !mFavoritePlaceList.isEmpty()) {
+            displayDetailFragment(mFavoritePlaceList.get(mSelectedPlaceIndex));
+        }
+        return true;
+    }
 }
